a510b968b52135b683411321fbca9dc507f9866a
suravitpop <pop.suravit29@gmail.com>
2021-10-19 00:42:24 +0700

    done login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import liff from '@line/liff'
+import {useState} from 'react';
 
 function App() {
+
+const [pictureUrl, SetPictureUrl] = useState(logo);
+const [idToken, SetIdToken] = useState("");
+const [statusMessage, SetStatusMessage] = useState("");
+const [displayName, SetDisplayName] = useState("");
+const [userId, SetUserId] = useState("");
+
+const logout = () =>{
+  liff.logout();
+  window.location.reload();
+
+}
+
+const initLine = () => {
+  liff.init({ liffId: '1656200256-Zrp28MK5' }, () => {
+    if (liff.isLoggedIn()) {
+      this.runApp();
+    } else {
+      liff.login();
+    }
+  }, err => console.error(err));
+}
+
+const runApp = () => {
+  const idToken = liff.getIDToken();
+  SetIdToken(idToken);
+  liff.getProfile().then(profile => {
+    console.log(profile);
+    SetDisplayName(profile.displayName);
+    SetIdToken(profile.idToken);
+    SetPictureUrl(profile.pictureUrl);
+    SetStatusMessage(profile.statusMessage);
+    SetUserId(profile.userId);
+  }).catch(err => console.error(err));
+}
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+<div style={{ textalign: "center" }}>
+  <h1>React with LINE Login</h1>
+  <img src={pictureUrl} width="300px" height="300px"/>
+  <p style={{ textalign: "left", marginleft: "20%" }}><b>ID token: </b> { idToken }</p>
+  <p style={{ textalign: "left", marginleft: "20%" }}><b>Display name: </b> { displayName }</p>
+  <p style={{ textalign: "left", marginleft: "20%" }}><b>Status message: </b> { statusMessage }</p>
+  <p style={{ textalign: "left", marginleft: "20%" }}><b>User ID: </b> { userId }</p>
+
+  <button onClick={() => logout()} style={{width: "100%", height:30 }}>Logout</button>
+</div>
       </header>
     </div>
   );
