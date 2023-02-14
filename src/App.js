6443d6634eeab5857e533da5b8d9c0a97b2d5dab
suravitpop <pop.suravit29@gmail.com>
2023-02-15 00:42:02 +0700

    v1 serve on ngrok

--- src/App.js
+++ src/App.js
@@ -1,65 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import liff from '@line/liff'
-import {useState,useEffect} from 'react';
+import React, { Component } from 'react';
 
-function App() {
+const liff = window.liff;
 
-const [pictureUrl, SetPictureUrl] = useState(logo);
-const [idToken, SetIdToken] = useState("");
-const [statusMessage, SetStatusMessage] = useState("");
-const [displayName, SetDisplayName] = useState("");
-const [userId, SetUserId] = useState("");
+class App extends Component {
 
-const logout = () =>{
-  liff.logout();
-  window.location.reload();
+  constructor(props) {
+    super(props);
 
-}
+    this.state = {
+      name: '',
+      userLineID: '',
+      pictureUrl: ''
+    };
+  }
 
-const initLine = () => {
-  liff.init({ liffId: '1656200256-Zrp28MK5' }, () => {
+  componentDidMount = async() => {
+    await liff.init({ liffId: '1656200256-Zrp28MK5' }).catch(err=>{throw err});
     if (liff.isLoggedIn()) {
-      runApp();
-    } else {
+      let getProfile = await liff.getProfile();
+      this.setState({
+        name: getProfile.displayName,
+        userLineID: getProfile.userId,
+        pictureUrl: getProfile.pictureUrl,
+      });
+    }else{
       liff.login();
     }
-  }, err => console.error(err));
-}
+  }
 
-const runApp = () => {
-  const idToken = liff.getIDToken();
-  SetIdToken(idToken);
-  liff.getProfile().then(profile => {
-    console.log(profile);
-    SetDisplayName(profile.displayName);
-    SetPictureUrl(profile.pictureUrl);
-    SetStatusMessage(profile.statusMessage);
-    SetUserId(profile.userId);
-  }).catch(err => console.error(err));
-}
-
-useEffect(() => {
-  initLine();
-}, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-<div style={{ textalign: "center" }}>
-  <h1>React with LINE Login</h1>
-  <img src={pictureUrl} width="300px" height="300px"/>
-  <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all"}}><b>ID token: </b> { idToken }</p>
-  <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>Display name: </b> { displayName }</p>
-  <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>Status message: </b> { statusMessage }</p>
-  <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>User ID: </b> { userId }</p>
-
-  <button onClick={() => logout()} style={{width: "100%", height:30 }}>Logout</button>
-</div>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="support">
+            <p>ชื่อ {this.state.name}</p>
+            <p>Line ID {this.state.userLineID}</p>
+            <img width="250" alt='pic' src={this.state.pictureUrl} />
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
